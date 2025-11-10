8c49a333c11827cb847fe87874d81348b5d5f404
Miguel <migueldco14@gmail.com>
2025-11-10 15:04:37 -0300

    fixed test

--- tests/kmp.cpp
+++ tests/kmp.cpp
@@ -3,6 +3,6 @@
 signed main() {
 	string s = "abcabcabc";
 	auto pre = kmp(s);
-	assert(pre == vector({0, 0, 0, 1, 2, 3, 4, 5, 1}));
+	assert(pre == vector({0, 0, 0, 1, 2, 3, 4, 5, 6}));
 	return 0;
 }
